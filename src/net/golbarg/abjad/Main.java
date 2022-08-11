76d32e7732ae000a3adbc41887f1c19df21ab174
Mustafa Poya <mustafapoyaabc@gmail.com>
2022-08-11 14:36:03 +0430

    load AbjadView in main

--- src/net/golbarg/abjad/Main.java
+++ src/net/golbarg/abjad/Main.java
@@ -1,25 +1,27 @@
 package net.golbarg.abjad;
-	
+
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
-
 
 public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			BorderPane root = new BorderPane();
-			Scene scene = new Scene(root,400,400);
+			BorderPane root = (BorderPane) FXMLLoader.load(getClass().getResource("AbjadView.fxml"));
+			Scene scene = new Scene(root, 320, 480);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
 			primaryStage.show();
-		} catch(Exception e) {
+			primaryStage.setResizable(false);
+
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
-	
+
 	public static void main(String[] args) {
 		launch(args);
 	}
